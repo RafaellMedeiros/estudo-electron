8daa385e8caa8fed1e0ade03efb01cf1659e3747
Rafael Medeiros <rafaelfufcg@gmail.com>
2026-06-26 23:33:39 -0300

    tutorial electron

---
+++ labs/analises/renderer.js
@@ -0,0 +1,13 @@
+const information = document.getElementById('info')
+
+console.log('Hello');
+
+
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node (v${versions.node()}) and Electron (v${versions.electron()})`
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response);
+}
+
+func()
